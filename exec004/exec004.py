1a785d7637e8e02bdf14802877bf30ec34f513e8
Arik Sosman <arik@sosman.eu>
2012-12-13 22:09:38 +0100

    fermat test implemented

--- exec004/exec004.py
+++ exec004/exec004.py
@@ -23,3 +23,55 @@ def pseudoRandom(maximum):
     lastRandomFile.close() # saving
 
     return newRandomNumber
+
+def fermatTest(allegedPrime, testCount):
+    for i in range(testCount):
+        a = (int)((random()*(allegedPrime-2))+1) # we generate an a satisfying 1 <= a < allegedPrime
+        indicator = pow(allegedPrime, a, allegedPrime-1)
+        isPrime = (gcd(indicator, allegedPrime) == 1)
+        if(isPrime == false):
+            return false
+    return true
+
+# old functions
+
+def calcBezoutIdentity(a, b):
+    r = [a, b]
+    s = [1, 0]
+    t = [0, 1]
+    q = ["null"]
+    i = 1
+    while r[i] > 0:
+        r.append(r[i-1]%r[i])
+        q.append(floor(r[i-1]/r[i]))
+        s.append(s[i-1]-q[i]*s[i])
+        t.append(t[i-1]-q[i]*t[i])
+        i = i+1
+    return r[i-1], s[i-1], t[i-1]
+
+def inv(a, n):
+    bezout = calcBezoutIdentity(a, n)
+    print bezout
+    gcd = bezout[0]
+    inverse = bezout[1]
+    print inverse, '*', a, ' + ', bezout[2], '*', n, ' = ', gcd
+    if gcd == 1:
+        return inverse
+    return false
+
+def pow(n, b, e):
+    necessaryFactors = []
+    currentExponent = e
+    currentBase = b
+    while true:
+        currentBase = currentBase%n
+        if currentExponent == 1:
+            break
+        if currentExponent%2 == 1:
+            necessaryFactors.append(currentBase)
+        currentExponent = floor(currentExponent/2)
+        currentBase = currentBase*currentBase
+    result = currentBase
+    for currentFactor in necessaryFactors:
+        result = result*currentFactor
+    return result%n
