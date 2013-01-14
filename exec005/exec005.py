8e91eaaee813b7de10847bc2fc256b3b162f870b
Arik Sosman <arik@sosman.eu>
2013-01-14 02:12:39 +0100

    latest version, though, admittedly, incomplete

--- exec005/exec005.py
+++ exec005/exec005.py
@@ -1,4 +1,4 @@
-def pollardRhoMethod(n):
+def pollardRhoMethod(n, cycleDetection):
     if(n%2 == 0):
         return 2
     x = 2
@@ -13,14 +13,17 @@ def pollardRhoMethod(n):
         d = gcd(abs(x-y), n)
     # print 'x =',x,'\ny =',y,'\nd =',d,'\n'
     print 'i =',i
-    if (d == n):
+    if (d == n and cycleDetection):
         print 'Failure'
         return false
     if (1<d and d < n):
         return d
 
 def fermatFactorization():
-    for n in range(5, 13):
+    for n in range(5, 7): # ueber 6 als Maximalzahl laeuft der Test zumindest auf meinem Computer zu langsam, deswegen
+                            # kann ich es derzeit nicht ganz ausfuehren. Muss ab 7 eine andere als die Rho-Methode
+                            # ausgefuehrt werden? Vorerst schicke ich meinen Code in seinem jetzigen Zustand ab, bitte
+                            # um Rueckmeldung
         fermatNumber = 2 ** (2 ** n) + 1
         # print fermatNumber
-        print pollardRhoMethod(fermatNumber)
+        print pollardRhoMethod(fermatNumber, true)
